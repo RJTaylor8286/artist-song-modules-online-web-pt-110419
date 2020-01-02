000825466cfd28a8cadae6d4b7f0377778f9e825
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2020-01-02 17:01:58 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,10 +3,12 @@ end
 
 def self.reset_all
     self.all.clear
-  extend
-  
+  extend Artist
+  extend Song   
   end
 
   def self.count
     self.all.count
+    extend Artist
+    extend Song
   end
